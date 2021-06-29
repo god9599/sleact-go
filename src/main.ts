4c6a4f7c5ec21c24c059e5012bb68af77467f65c
god9599 <59276134+god9599@users.noreply.github.com>
2021-06-30 02:32:15 +0900

    [Env] swagger 연동

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
@@ -6,6 +7,14 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  const config = new DocumentBuilder()
+    .setTitle('Sleact API')
+    .setDescription('Sleact 개발을 위한 API 문서')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
   console.log(`listening on port ${port}`);
   if (module.hot) {
