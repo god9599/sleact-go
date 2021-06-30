9de0aff57a414177b5bf49f2ee19190d79b7022d
god9599 <59276134+god9599@users.noreply.github.com>
2021-06-30 18:39:40 +0900

    [Etc] 커스텀 데코레이터 생성

---
+++ src/common/decorator/token.decorator.ts
@@ -0,0 +1,10 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const Token = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const response = ctx.switchToHttp().getResponse();
+    return response.locals.jwt;
+  },
+);
+
+// @Token() token으로 사용 가능
