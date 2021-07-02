72ca086eed4968a2028db0144a55362d2e7459e7
god9599 <59276134+god9599@users.noreply.github.com>
2021-07-03 01:48:36 +0900

    [Refact] decorator로 트랜잭션 활용

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,7 +4,12 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, Connection, QueryRunner } from 'typeorm';
+import {
+  Repository,
+  Connection,
+  Transaction,
+  TransactionRepository,
+} from 'typeorm';
 import { Users } from '../entities/user.entity';
 import bcrypt from 'bcrypt';
 import { WorkspaceMembers } from '../entities/workspaceMembers.entity';
@@ -19,38 +24,39 @@ export class UsersService {
     private workspaceMembersRepository: Repository<WorkspaceMembers>,
     @InjectRepository(ChannelMembers)
     private channelMembersRepository: Repository<ChannelMembers>,
-    private connection: Connection,
   ) {}
 
   // async getUser() {}
 
-  async join(email: string, nickname: string, password: string) {
-    const queryRunner = this.connection.createQueryRunner();
-    queryRunner.connect();
-    const user = await this.usersRepository.findOne({ where: { email } });
+  @Transaction()
+  async join(
+    email: string,
+    nickname: string,
+    password: string,
+    @TransactionRepository(Users) usersRepository: Repository<Users>,
+    @TransactionRepository(Users)
+    workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @TransactionRepository(Users)
+    channelMembersRepository: Repository<ChannelMembers>,
+  ) {
+    const user = await usersRepository.findOne({ where: { email } });
     if (user) {
       throw new UnauthorizedException('이미 존재하는 사용자입니다.');
     }
     const hashedPassword = await bcrypt.hash(password, 12);
-    try {
-      const returned = await this.usersRepository.save({
-        email,
-        nickname,
-        password: hashedPassword,
-      });
-      await this.workspaceMembersRepository.save({
-        userId: returned.id,
-        workspaceId: 1,
-      });
-      await this.channelMembersRepository.save({
-        userId: returned.id,
-        channelId: 1,
-      });
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-    } finally {
-      await queryRunner.release();
-    }
+    const returned = await usersRepository.save({
+      email,
+      nickname,
+      password: hashedPassword,
+    });
+    await workspaceMembersRepository.save({
+      userId: returned.id,
+      workspaceId: 1,
+    });
+    await channelMembersRepository.save({
+      userId: returned.id,
+      channelId: 1,
+    });
     return true;
   }
 }
