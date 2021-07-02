0f8c9747a7366b546ca2e9bb891b61bcaf24653f
god9599 <59276134+god9599@users.noreply.github.com>
2021-07-03 01:33:34 +0900

    [Etc] 로그인 시 기본 데이터 적용 및 트랜잭션 활용

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,13 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from '../entities/user.entity';
+import { WorkspaceMembers } from '../entities/workspaceMembers.entity';
+import { ChannelMembers } from 'src/entities/channelMembers.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])], // 레포지토리를 service에 injection
+  imports: [
+    TypeOrmModule.forFeature([Users, WorkspaceMembers, ChannelMembers]),
+  ], // 레포지토리를 service에 injection
   providers: [UsersService],
   controllers: [UsersController],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,29 +4,53 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, Connection, QueryRunner } from 'typeorm';
 import { Users } from '../entities/user.entity';
 import bcrypt from 'bcrypt';
+import { WorkspaceMembers } from '../entities/workspaceMembers.entity';
+import { ChannelMembers } from '../entities/channelMembers.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users)
     private usersRepository: Repository<Users>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
+    private connection: Connection,
   ) {}
 
   // async getUser() {}
 
   async join(email: string, nickname: string, password: string) {
+    const queryRunner = this.connection.createQueryRunner();
+    queryRunner.connect();
     const user = await this.usersRepository.findOne({ where: { email } });
     if (user) {
       throw new UnauthorizedException('이미 존재하는 사용자입니다.');
     }
     const hashedPassword = await bcrypt.hash(password, 12);
-    await this.usersRepository.save({
-      email,
-      nickname,
-      password: hashedPassword,
-    });
+    try {
+      const returned = await this.usersRepository.save({
+        email,
+        nickname,
+        password: hashedPassword,
+      });
+      await this.workspaceMembersRepository.save({
+        userId: returned.id,
+        workspaceId: 1,
+      });
+      await this.channelMembersRepository.save({
+        userId: returned.id,
+        channelId: 1,
+      });
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
+    return true;
   }
 }
